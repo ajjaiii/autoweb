10ad8fe796abb6635a1379658e082ef7f228da9e
Ahmad Aryadi <ahmad.aryadi24@yahoo.com>
2019-02-21 08:02:15 +0000

    commit pertama

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+jawaban="y"
+read -p "Apakah kamu yakin akan menginstall webserver ? (Y/N)" pilih;
+
+if [ $pilih == $jawaban ];
+then
+	echo "Menyiapkan Instalasi Web Server"
+	sudo apt-get update
+	echo "Melakukan instalasi Web Server"
+	sudo apt-get install -y apache2 php php-mysql
+	echo "melakukan instalasi Database server"
+	sudo apt-get install -y mysql-server
+	echo "========================================="
+	echo "=============Instalasi Selesai==========="
+	echo "========================================="
+	exit 0 
+else
+	echo "Instalasi dibatalkan"
+	exit 1
+fi
---
+++ web-app.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+jawaban="y"
+
+read -p "apakah kamu yakin akan melakukan setup aplikasi web ? (Y/N)" pilih;
+
+if [ $pilih == $jawaban ];
+then
+	echo "==============================="
+	echo "Downloading Data"
+	echo "==============================="
+	cd
+	wget https://github.com/sdcilsy/sosial-media/archive/master.zip
+	echo "==============================="
+	echo "Ekstrak File"
+	echo "==============================="
+	unzip master.zip
+	echo "==============================="
+	echo "memindahkan data"
+	echo "==============================="
+	sudo rm -Rf /var/www/html/*
+	sudo rm -f /var/www/html/*
+	sudo mv sosial-media-master/* /var/www/html
+	echo "setup selesai"
+	exit 0
+else
+	echo "setup dibatalkan"
+	exit 1
+fi
